0637b7926d8cc83a67c0a16db4dec23b056a091c
Sergio Arribas <arribastellezsergio@gmail.com>
2024-08-02 17:52:59 +0000

    feat domain

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+
+function generateDomains(pronoun, adj, noun) {
+  let domains = [];
+  
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        domains.push(p + a + n + ".com");
+      }
+    }
+  }
+  
+  return domains;
+}
+
+
+let domainList = generateDomains(pronoun, adj, noun);
+domainList.forEach(domain => console.log(domain));
+let myTag = document.querySelector("Dominios")
+
 };
